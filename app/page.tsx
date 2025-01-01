ac56eb6878f11a3b2b721591a47b4c3501893408
Chao Zheng <zhengchao.qqqqq@gmail.com>
2025-01-01 13:38:47 +0800

    dev branch

--- app/page.tsx
+++ app/page.tsx
@@ -33,7 +33,7 @@ export default function App() {
 
   return (
     <main>
-      <h1>My todos</h1>
+      <h1>zc todos</h1>
       <button onClick={createTodo}>+ new</button>
       <ul>
         {todos.map((todo) => (
