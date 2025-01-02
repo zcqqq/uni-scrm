61909383676be4b95b605946265c6d0414393e1f
Chao Zheng <zhengchao.qqqqq@gmail.com>
2025-01-02 23:34:41 +0800

    function to call AppSync

--- amplify/functions/tiktok/index.ts
+++ amplify/functions/tiktok/index.ts
@@ -1,8 +1,12 @@
 import type { Handler } from 'aws-lambda';
 import authorize from './Login/authorize';
 import { Amplify } from 'aws-amplify';
-import outputs from '../../../amplify_outputs.json';
-Amplify.configure(outputs);
+import { getAmplifyDataClientConfig } from '@aws-amplify/backend/function/runtime';
+import { env } from "$amplify/env/tiktok";
+const { resourceConfig, libraryOptions } = await getAmplifyDataClientConfig(
+  env
+);
+Amplify.configure(resourceConfig, libraryOptions);
 
 export const handler: Handler = async (event) => {
     if (event.requestContext.http.method === 'GET') {
