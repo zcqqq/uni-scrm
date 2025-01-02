06b03c5c793776388e3d886c800f33314bf8e1e1
Chao Zheng <zhengchao.qqqqq@gmail.com>
2025-01-02 11:12:11 +0800

    added SSR

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import { AntdRegistry } from '@ant-design/nextjs-registry';
+import ConfigureAmplifyClientSide from "@/components/ConfigureAmplify";
+
 
 const RootLayout = ({ children }: React.PropsWithChildren) => (
   <html lang="en">
     <body>
-      <AntdRegistry>{children}</AntdRegistry>
+      <AntdRegistry>        <ConfigureAmplifyClientSide />
+        {children}</AntdRegistry>
     </body>
   </html>
 );
---
+++ app/login/page.tsx
@@ -0,0 +1,6 @@
+// app/login/page.tsx
+import Login from "@/components/Login";
+
+export default function LoginPage() {
+  return <Login />;
+}
